8d96e29eefe489a3b4cfd23151ee0fff5a29cba1
Siddharth Singh <me@shantaram.xyz>
2026-03-12 20:28:29 +0530

    add Util.show_waypoint

--- scripts/util.lua
+++ scripts/util.lua
@@ -13,3 +13,44 @@ function Util.has_value(tab, val)
 
     return false
 end
+
+function Util.get_waypoint_text_size(text, font_size)
+    local char_width = font_size or 12
+    return {
+        x = #text * char_width + 10, -- add padding
+        y = 20
+    }
+end
+
+function Util.show_waypoint(player, pos, text, show_distance)
+    local waypoint_id = player:hud_add({
+        type = "waypoint",
+        name = text,
+        world_pos = pos,
+        text = show_distance and "m" or nil,
+        precision = show_distance and 2 or 0,
+        number = 0xFFFFFF
+    })
+
+    local bg_id = player:hud_add({
+        type = "image_waypoint",
+        world_pos = pos,
+        text = "waypoint_bg.png",
+        z_index = -300,
+        scale = Util.get_waypoint_text_size(text)
+    })
+
+    local removed = false
+
+    local function remove()
+        if removed then
+            return
+        end
+        removed = true
+
+        player:hud_remove(waypoint_id)
+        player:hud_remove(bg_id)
+    end
+
+    return remove
+end
